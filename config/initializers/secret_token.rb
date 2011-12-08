ebae8872ed0092c109a092057357a3abad0df677
RichardsonDx <richardsondx@gmail.com>
2011-12-07 19:34:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TreepollA::Application.config.secret_token = '4866378a1ec4f7fc1934a2ec669decb02d79e5826c7f1d6124c666f4c568aa1665360455dc0c7968e38c14422438605a6782b17ba02ca6692d3e3df1ec6876f9'
